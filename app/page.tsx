9b1ec9bd3c6226de7c56db130df99243ca11a226
Pablo <pablomoura2016@gmail.com>
2025-05-20 21:44:33 -0300

    adiciona componente Welcome à página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Welcome } from "./Welcome";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Welcome />
     </div>
   );
 }
