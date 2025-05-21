a48ba9298689290af56c8687cae573ad8e2ac6f2
Pablo <pablomoura2016@gmail.com>
2025-05-20 21:42:09 -0300

    adiciona componente Button e função Welcome; atualiza dependências do Radix UI

---
+++ app/Welcome.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export function Welcome(){
+
+    return (
+        <section className="flex flex-col items-center">
+            <Button>
+                teste
+            </Button>
+        </section>
+    )
+}
