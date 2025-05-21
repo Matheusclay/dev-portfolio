a90ac01cb3acfe4e20da452b700c294e8b34eb33
Deigosz <rodriguesdiego412@gmail.com>
2025-05-21 20:18:13 -0300

    AB#189 Criando Card Genericos para colocar Skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { MainMenu } from "./components/MainMenu"
-
+import { CardWithForm } from "./components/CardSkills"
 export default function Home() {
   return (
     <main>
       <div className="justify-center items-center flex h-[10vh] ">
+
         <MainMenu  />
+      </div>
+      <div>
+        <CardWithForm/>
       </div>
     </main>
   );
